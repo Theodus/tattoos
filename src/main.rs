c2759bc3d458a4c6ce3297c5efe8140ee5444ee9
Theo Butler <theodusbutler@gmail.com>
2022-05-07 21:21:00 -0400

    Middle rings

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> Result {
     ctx.set_line_width(half * 0.02);
     ctx.arc(0.0, 0.0, half * 0.97, 0.0, PI * 2.0);
     ctx.stroke()?;
-    radial_repeat(&ctx, 12, 0.0, |ctx| {
+    radial_repeat(&ctx, 12, PI / 12.0, |ctx| {
         ctx.translate(0.0, half * 0.66);
         ctx.set_line_width(half * 0.012);
         ctx.rectangle(-half * 0.02, half * 0.001, half * 0.04, half * 0.04);
@@ -33,16 +33,64 @@ fn main() -> Result {
         ctx.translate(0.0, half * 0.08);
         isosceles_triangle_stroke(&ctx, half * 0.08, PI / 3.4)?;
         ctx.translate(0.0, half * 0.08);
-        isosceles_triangle_stroke(&ctx, half * 0.08, PI / 3.4)
+        isosceles_triangle_stroke(&ctx, half * 0.08, PI / 3.4)?;
+        Ok(())
     })?;
-    radial_repeat(&ctx, 12, PI / 12.0, |ctx| {
+    radial_repeat(&ctx, 12, 0.0, |ctx| {
         ctx.translate(0.0, half * 0.76);
         ctx.set_line_width(half * 0.03);
         ctx.arc(0.0, 0.0, half * 0.05, 0.0, PI * 2.0);
         ctx.stroke()?;
         ctx.set_line_width(half * 0.02);
         ctx.arc(0.0, 0.0, half * 0.085, 0.0, PI * 2.0);
-        ctx.stroke()
+        ctx.stroke()?;
+        Ok(())
+    })?;
+    ctx.set_line_width(half * 0.022);
+    ctx.arc(0.0, 0.0, half * 0.664, 0.0, PI * 2.0);
+    ctx.stroke()?;
+    radial_repeat(&ctx, 6, 0.0, |ctx| {
+        ctx.translate(0.0, half * 0.41);
+        ctx.set_line_width(half * 0.022);
+        ctx.arc(0.0, 0.0, half * 0.24, 0.0, PI * 2.0);
+        ctx.stroke()?;
+        ctx.arc(0.0, 0.0, half * 0.1, 0.0, PI * 2.0);
+        ctx.stroke()?;
+        ctx.set_line_width(half * 0.016);
+        ctx.arc(0.0, 0.0, half * 0.2, 0.0, PI);
+        ctx.stroke()?;
+        ctx.translate(0.0, half * 0.035);
+        ctx.set_line_width(half * 0.016);
+        isosceles_triangle_stroke(&ctx, -half * 0.11, PI / 3.0)?;
+        let line_x = half * 0.16;
+        ctx.move_to(0.0, half * 0.06);
+        ctx.line_to(0.0, line_x);
+        ctx.move_to(0.0, 0.0);
+        ctx.line_to(line_x, line_x * (PI / 6.0).tan());
+        ctx.move_to(0.0, 0.0);
+        ctx.line_to(-line_x, line_x * (PI / 6.0).tan());
+        let tangent_x = half * 0.031;
+        ctx.translate(tangent_x, -tangent_x / (PI / 6.0).tan());
+        ctx.move_to(0.0, 0.0);
+        ctx.line_to(line_x, line_x * (PI / 6.0).tan());
+        ctx.move_to(0.0, 0.0);
+        ctx.rel_line_to(0.0, -half * 0.22);
+        ctx.translate(-tangent_x * 2.0, 0.0);
+        ctx.move_to(0.0, 0.0);
+        ctx.line_to(-line_x, line_x * (PI / 6.0).tan());
+        ctx.move_to(0.0, 0.0);
+        ctx.rel_line_to(0.0, -half * 0.22);
+        ctx.stroke()?;
+        Ok(())
+    })?;
+    radial_repeat(&ctx, 6, PI / 6.0, |ctx| {
+        ctx.translate(0.0, half * 0.355);
+        ctx.set_line_width(half * 0.015);
+        ctx.arc(0.0, 0.0, half * 0.035, 0.0, PI * 2.0);
+        ctx.stroke()?;
+        ctx.arc(0.0, 0.0, half * 0.11, 0.0, PI * 2.0);
+        ctx.stroke()?;
+        Ok(())
     })?;
 
     Ok(())
